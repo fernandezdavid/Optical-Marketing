42e0ef39985568588fc45fc4241194be94287f9c
fernandezdavid <fernandezdavideduardo@gmail.com>
2013-07-14 02:53:50 -0300

    AdvertDetails/advert interaction classes added

---
+++ OMKT/Business/AdvertDetailInteraction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OMKT.Business
+{
+    public class AdvertDetailInteraction
+    {
+        public int AdvertDetailInteractionID { get; set; }
+
+        public int AdvertDetailID { get; set; }
+        public virtual AdvertDetail AdvertDetail { get; set; }
+
+        public Boolean View { get; set; }
+
+        public Boolean Like { get; set; }
+    }
+}
---
+++ OMKT/Business/AdvertInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OMKT.Business
+{
+    public class AdvertInteraction
+    {
+        public int AdvertInteractionID { get; set; }
+
+        public int AdvertID { get; set; }
+        public virtual Advert Advert { get; set; }
+
+        public double Height { get; set; }
+
+        public DateTime? StartDatetime { get; set; }
+
+        public DateTime? EndDatetime { get; set; }
+
+        public ICollection<AdvertDetailInteraction> AdvertDetailInteractions { get; set; }
+
+        public AdvertInteraction()
+        {
+            AdvertDetailInteractions = new List<AdvertDetailInteraction>();
+        }
+
+     }
+}
