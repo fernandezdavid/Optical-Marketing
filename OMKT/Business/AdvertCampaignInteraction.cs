a25e368360642cb0129f949ef5ba4690edb4873f
fernandezdavid <fernandezdavideduardo@gmail.com>
2013-07-19 00:33:13 -0300

    AdvertCampaignInteraction class added

---
+++ OMKT/Business/AdvertCampaignInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OMKT.Business
+{
+    public class AdvertCampaignInteraction
+    {
+        public int AdvertCampaignInteractionID { get; set; }
+
+        public int AdvertCampaignID { get; set; }
+        public virtual AdvertCampaign AdvertCampaign { get; set; }
+
+        public DateTime? StartDatetime { get; set; }
+
+        public DateTime? EndDatetime { get; set; }
+
+        public ICollection<AdvertInteraction> AdvertInteractions { get; set; }
+
+        public AdvertCampaignInteraction()
+        {
+            AdvertInteractions = new List<AdvertInteraction>();
+        }
+    }
+}
